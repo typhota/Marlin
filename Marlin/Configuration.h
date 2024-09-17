04e37683fcd6b39801c849d7de01bceeba40e0b2
typhota <149052638+typhota@users.noreply.github.com>
2024-09-16 20:16:24 -0700

    Updated configuration

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -20,8 +20,8 @@
  *
  */
 #pragma once
-#error "Don't build with import-2.1.x configurations!"
-#error "Use the 'bugfix...' or 'release...' configurations matching your Marlin version."
+
+#define CONFIG_EXAMPLES_DIR "Creality/Ender-3 Pro/BigTreeTech SKR Mini E3 3.0"
 
 /**
  * Configuration.h
